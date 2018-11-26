5b67fc8e2c1b263612968f8ef7432384e2e4cccc
tranhieu97 <43314948+tranhieu97@users.noreply.github.com>
2018-11-27 00:43:21 +0700

    h4

--- app/src/main/java/com/itk16/bk/pc/appluyentoanlop1/StartActivity.java
+++ app/src/main/java/com/itk16/bk/pc/appluyentoanlop1/StartActivity.java
@@ -1,8 +1,10 @@
 package com.itk16.bk.pc.appluyentoanlop1;
 
 import android.app.Dialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.os.Bundle;
@@ -11,6 +13,7 @@ import android.support.v7.app.AlertDialog.Builder;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import java.util.Locale;
 
@@ -20,7 +23,16 @@ public class StartActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        SharedPreferences sharedPreferences= this.getSharedPreferences("KEY", Context.MODE_PRIVATE);
+        language=sharedPreferences.getString("LG", "en");
+        Locale locale= new Locale(language);
+        Locale.setDefault(locale);
+        Resources resources= getResources();
+        Configuration con = resources.getConfiguration();
+        con.locale= locale;
+        resources.updateConfiguration(con, resources.getDisplayMetrics());
         setContentView(R.layout.activity_start);
+        Toast.makeText(this,language, Toast.LENGTH_SHORT).show();
         bt = (Button) findViewById(R.id.bt_bd);
         bt_set_language=(Button)findViewById(R.id.settinglanguage) ;
         setevent();
@@ -30,10 +42,10 @@ public class StartActivity extends AppCompatActivity {
     {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("Setting Language");
-        builder.setPositiveButton("Vietnamese", new DialogInterface.OnClickListener() {
+        builder.setNegativeButton("Vietnamese", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                language= "vn";
+                language= "vi";
                 setLanguage(language);
                 dialog.cancel();
             }
@@ -67,11 +79,17 @@ public class StartActivity extends AppCompatActivity {
     {
         Locale locale= new Locale(lg);
         Locale.setDefault(locale);
-
         Resources resources= getResources();
         Configuration con = resources.getConfiguration();
         con.locale= locale;
         resources.updateConfiguration(con, resources.getDisplayMetrics());
+        SharedPreferences sharedPreferences= this.getSharedPreferences("KEY", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("LG", language);
+        editor.apply();
+        Intent intent = new Intent(StartActivity.this,StartActivity.class);
+        startActivity(intent);
+        finish();
     }
 
 }
